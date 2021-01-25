4309ddfc5a58114dfd8babd126e3a4641a8d84de
Islanilton <islanilton.rodrigues@gmail.com>
2021-01-25 09:20:01 -0300

    unform integrated

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, KeyboardAvoidingView, ScrollView, Platform, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
-import {useNavigation} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core'
 
 import logoImg from '../../assets/logo.png';
 
@@ -17,6 +19,12 @@ import {
 
 const Signup: React.FC = () => {
   const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSignUp = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+
   return (
     <>
       <KeyboardAvoidingView
@@ -34,11 +42,17 @@ const Signup: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name="name" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input name="name" icon="user" placeholder="Nome" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+            </Form>
 
-            <Button onPress={() => {console.log('SHOW')}}>Entrar</Button>
+            <Button onPress={() => {
+              formRef.current?.submitForm();
+            }}>
+              Entrar
+            </Button>
 
           </Container>
         </ScrollView>
